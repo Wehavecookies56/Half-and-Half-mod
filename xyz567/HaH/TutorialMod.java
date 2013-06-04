0e2d71a2cbc07f34716dfd1ac9d06cc071c36994
xyz5674 <xyz567@outlook.com>
2013-06-04 20:33:13 +0100

    Config version

--- xyz567/HaH/TutorialMod.java
+++ xyz567/HaH/TutorialMod.java
@@ -15,12 +15,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.EnumHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraftforge.common.Configuration;
 
 /*
 * Basic needed forge stuff
@@ -42,6 +45,24 @@ public class TutorialMod {
 	public static Block Golddiamond3;
 	public static Block Golddiamond4;
 	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event){
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		
+		config.load();
+		
+        int golddiamondID = config.getBlock("Golddiamond", 3611).getInt();
+        
+        int golddiamondoppositeID = config.getBlock("Golddiamondopposite", 3612).getInt();
+        
+        int golddiamond3ID = config.getBlock("Golddiamond3", 3613).getInt();
+        
+        int golddiamond4ID = config.getBlock("Golddiamond4", 3614).getInt();
+
+        int GoldDiamondIngotID = config.getItem("GoldDiamondIngot", 3678).getInt();
+		
+		config.save();
+	}
 	//declaring Init
 	@Init
 	public void load(FMLInitializationEvent event){
